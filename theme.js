ae15b8206ab43c8de724e65687e376a4c1427feb
Pieter <pieter@devpieter.nl>
2026-02-11 18:47:10 +0100

    Now playing bar rework; Cleanup; QoL

--- theme.js
+++ theme.js
@@ -1,5 +1,6 @@
 (function bloom() {
 
+  /* == Utility functions == */
   function waitForElements(elementSelectors, func, attempts = 50) {
     const queries = elementSelectors.map((elementSelector) => document.querySelector(elementSelector));
 
@@ -8,63 +9,29 @@
       return;
     }
 
-    if (attempts > 0) {
-      setTimeout(waitForElements, 200, elementSelectors, func, attempts - 1);
-    }
+    if (attempts <= 0) return;
+    setTimeout(waitForElements, 100, elementSelectors, func, attempts - 1);
   }
 
-  waitForElements(
-    // Move the 'Home' button from the left of the search bar to the other nav elements on the left
-    [".main-globalNav-searchContainer button", ".main-globalNav-historyButtons"],
-    ([searchButton, navLinksContainer]) => navLinksContainer.appendChild(searchButton)
-  );
+  function ensureContentSpacer(node, id, height, prepend, waitForSelector, attempts = 50) {
+    if (!node) return;
 
-  const registeredSpacers = new Set();
+    const existingSpacer = node.querySelector(`#${id}`);
+    if (existingSpacer) {
+      existingSpacer.style.height = height;
+      return;
+    }
 
-  registeredSpacers.add({
-    id: "main-home-content__spacer",
-    nodeSelector: ".main-home-content",
-    height: "8px",
-    prepend: true
-  });
+    if (waitForSelector) {
+      const waitForNode = node.querySelector(waitForSelector);
 
-  registeredSpacers.add({
-    id: "main-yourLibraryX-libraryRootlist__spacer",
-    nodeSelector: ".main-yourLibraryX-libraryRootlist",
-    height: "100px",
-    prepend: false
-  });
+      if (!waitForNode) {
+        if (attempts <= 0) return;
 
-  // Right sidebar (now playing, queue, etc.)
-  registeredSpacers.add({
-    id: "iHa_q9pq4un3VNRQgwTx__spacer",
-    nodeSelector: ".iHa_q9pq4un3VNRQgwTx",
-    height: "50px",
-    prepend: false
-  });
-
-  const observer = new MutationObserver((mutations) => {
-    for (const mutation of mutations) {
-      for (const node of mutation.addedNodes) {
-        if (!(node instanceof HTMLElement)) continue;
-
-        for (const spacer of registeredSpacers) {
-          if (node.matches?.(spacer.nodeSelector)) ensureContentSpacer(node, spacer.id, spacer.height, spacer.prepend);
-
-          const innerNode = node.querySelector?.(spacer.nodeSelector);
-          if (innerNode) ensureContentSpacer(innerNode, spacer.id, spacer.height, spacer.prepend);
-        }
+        setTimeout(ensureContentSpacer, 100, node, id, height, prepend, waitForSelector, attempts - 1);
+        return;
       }
     }
-  });
-
-  observer.observe(document.body, {
-    childList: true,
-    subtree: true
-  });
-
-  function ensureContentSpacer(node, id, height, prepend) {
-    if (!node || node.querySelector(`#${id}`)) return;
 
     const spacer = document.createElement("div");
     spacer.id = id;
@@ -74,4 +41,67 @@
     if (prepend) node.prepend(spacer);
     else node.appendChild(spacer);
   }
+
+  /* == Init == */
+  waitForElements(
+    // Move the 'Home' button from the left of the search bar to the other nav elements on the left
+    [".main-globalNav-searchContainer button", ".main-globalNav-historyButtons"],
+    ([searchButton, navLinksContainer]) => navLinksContainer.appendChild(searchButton)
+  );
+
+  waitForElements(
+    [".Root__now-playing-bar", ".Root__main-view"],
+    ([_, mainView]) => {
+      alignNowPlayingBar();
+      new ResizeObserver(() => alignNowPlayingBar()).observe(mainView);
+    }
+  );
+
+  const alignNowPlayingBar = () => {
+    // Move the now playing bar to align with the main content instead of being full width
+    const nowPlayingBar = document.querySelector(".Root__now-playing-bar");
+    const mainView = document.querySelector(".Root__main-view");
+
+    if (!nowPlayingBar || !mainView) return;
+
+    nowPlayingBar.style.width = `calc(${mainView.clientWidth}px - calc(var(--now-playing-bar-margin) * 2))`;
+    nowPlayingBar.style.left = `calc(${mainView.getBoundingClientRect().left}px + var(--now-playing-bar-margin))`;
+  };
+
+  /* == Content spacers == */
+  const registeredSpacers = new Set();
+
+  registeredSpacers.add({
+    id: "main-home-content__spacer",
+    nodeSelector: ".main-home-content",
+    height: "8px",
+    prepend: true,
+    waitForSelector: undefined
+  });
+
+  registeredSpacers.add({
+    id: "main-view-container__scroll-node-child__spacer",
+    nodeSelector: ".main-view-container__scroll-node-child > main > section",
+    height: "100px",
+    prepend: false,
+    waitForSelector: ".main-view-container__scroll-node-child > main > section > div:not(.main-loadingPage-container)"
+  });
+
+  new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+      for (const node of mutation.addedNodes) {
+        if (!(node instanceof HTMLElement)) continue;
+
+        for (const spacer of registeredSpacers) {
+          if (node.matches?.(spacer.nodeSelector)) ensureContentSpacer(node, spacer.id, spacer.height, spacer.prepend, spacer.waitForSelector);
+
+          const innerNode = node.querySelector?.(spacer.nodeSelector);
+          if (innerNode) ensureContentSpacer(innerNode, spacer.id, spacer.height, spacer.prepend, spacer.waitForSelector);
+        }
+      }
+    }
+  }).observe(document.body, {
+    childList: true,
+    subtree: true
+  });
 })();
